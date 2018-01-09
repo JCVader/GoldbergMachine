fcfee1763d93c2ef7eff8547bee1af233a0f0975
JCVader <dawidpszeniczny18@gmail.com>
2018-01-09 13:25:10 +0100

    Goldberg Machine Update
    First idea

---
+++ Assets/SpeedUpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpScript : MonoBehaviour 
+{
+	Vector3 startPosition;
+	void Start () 
+	{
+		Time.timeScale = 10;
+		startPosition = transform.position;
+
+	}
+	
+	void Update () 
+	{
+		if (Vector3.Distance (transform.position, startPosition) > 0.1f) 
+		{
+			Time.timeScale = 1;
+		}
+		
+	}
+}
